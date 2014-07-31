7a1fff1e2579866e97ae39b72be9997588c30038
Mehmet <Mehmet@elite>
2014-07-31 20:48:14 +0300

    unnecessary import removed

--- SpringJavaFXController/src/main/java/com/sunkur/springjavafxcontroller/config/AppContextConfig.java
+++ SpringJavaFXController/src/main/java/com/sunkur/springjavafxcontroller/config/AppContextConfig.java
@@ -6,17 +6,14 @@
 
 package com.sunkur.springjavafxcontroller.config;
 
-import com.sunkur.springjavafxcontroller.screen.ScreensContoller;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  *
  * @author Mehmet Sunkur <mehmetsunkur@gmail.com>
  */
 @Configuration
-@Import(ScreensContoller.class)
 @ComponentScan("com.sunkur.springjavafxcontroller")
 public class AppContextConfig {
 
